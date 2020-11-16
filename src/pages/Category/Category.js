bfb08c99c3dd2626e48b741bfb3f10f3b1f100bc
Xednicoder <kaaiinn4@gmail.com>
2020-11-17 00:29:04 +0900

    Setting : sass version downgrade

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import '../../styles/common.scss';
+import '../../styles/reset.scss';
 
 class Category extends Component {
   render() {
-    return <div></div>;
+    return (
+      <section className='categoryContainer'>
+        <asid className='categorySideBarContainer'>
+          <ul>
+            <li className='categoryList'>가구</li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+            <li className='categoryList'></li>
+          </ul>
+        </asid>
+        <div className='categoryBannerbar'></div>
+        <div className='categoryFeedContainer'></div>
+      </section>
+    );
   }
 }
 
