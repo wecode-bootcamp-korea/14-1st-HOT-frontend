39d66db1e567e7f111b0c2b8d04437a773471755
공주민 <ley67890@naver.com>
2020-11-18 21:20:00 +0900

    Modify : NavBar 레이아웃 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Community from './pages/Community/Community';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Community from "./pages/Community/Community";
+import Nav from "./component/NavigationBar/NavigationBar";
 
 class Routes extends React.Component {
   render() {
@@ -8,7 +9,8 @@ class Routes extends React.Component {
       <>
         <Router>
           <Switch>
-            <Route exact path='/' component={Community} />
+            <Route exact path="/" component={Community} />
+            <Route exact path="/nav" component={Nav} />
           </Switch>
         </Router>
       </>
