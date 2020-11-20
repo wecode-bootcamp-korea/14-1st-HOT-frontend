19beddbd27dd86012794cdb18aa1ad988ba69a2d
Xednicoder <kaaiinn4@gmail.com>
2020-11-20 22:45:33 +0900

    Modify : Overview layout

--- src/pages/ProductDetail/Overview/Overview.js
+++ src/pages/ProductDetail/Overview/Overview.js
@@ -4,17 +4,17 @@ import './Overview.scss';
 class Summary extends Component {
   render() {
     return (
-      <div className='Overview'>
-        <div className='product'>
+      <div className='overview'>
+        <div className='overviewBox'>
           <div className='category'>
             {'가구'} {'>'} {'소파/거실가구'} {'>'} {'소파'} {'>'} {'일반소파'}
           </div>
-          <div className='production'>
+          <div className='overviewCover'>
             <div className='coverImageBox'>
               <div className='coverImageListBox'>zz</div>
               <div className='coverImageWrap'>xx</div>
             </div>
-            <div className='overViewBox'>
+            <div className='overViewList'>
               <div className='seller'>zz</div>
               <div className='title'>xx</div>
               <div className='review'>
