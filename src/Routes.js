3a4d1b7d909e1dffeb0030632499c8d939cf3f0f
공주민 <ley67890@naver.com>
2020-11-21 17:54:18 +0900

    Modify : 내용 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Community from './pages/Community/Community';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Community from "./pages/Community/Community";
+import Posting from "./pages/Posting/Posting";
 
 class Routes extends React.Component {
   render() {
@@ -8,7 +9,8 @@ class Routes extends React.Component {
       <>
         <Router>
           <Switch>
-            <Route exact path='/' component={Community} />
+            <Route exact path="/" component={Community} />
+            <Route exact path="/posting" component={Posting} />
           </Switch>
         </Router>
       </>
