44e0e4be5adb8fb004219201b2155af9c92e3f87
공주민 <ley67890@naver.com>
2020-11-22 21:39:45 +0900

    Add : add fetch

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,23 +1,74 @@
 import React, { Component } from "react";
 import "./Signup.scss";
 import Simplesignup from "./Simplesignup.js";
-import Select from "./Select";
+
+// const API = "http://10.58.0.125:8000/user/register";
 
 class Signup extends Component {
+  state = {
+    data: {},
+  };
+
   constructor() {
     super();
     this.state = {
-      idValue: "",
+      id: "",
+      pw: "",
+      name: "",
+      // passwordcheck: "",
     };
   }
 
+  componentDidMount() {
+    this.handleClick();
+  }
+
+  handleClick = (e) => {
+    const { id, pw, name } = this.state;
+
+    fetch("http://10.58.0.125:8000/user/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: id,
+        password: pw,
+        username: name,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+
+    // if (this.state.data.message === "success") {
+    //   alert("회원가입이 완료되었습니다.");
+    //   this.props.history.push("/");
+    // } else {
+    //   alert(this.state.data.message);
+    // }
+  };
+
   handleChangeEmail = (e) => {
     const { value } = e.target;
-    this.setState({ idValue: value });
+    this.setState({ id: value });
+  };
+
+  handleChangePw = (e) => {
+    const { value } = e.target;
+    this.setState({ pw: value });
+  };
+
+  // handleChangePwCheck = (e) => {
+  //   const { value } = e.target;
+  //   this.setState({ passwordcheck: value });
+  // };
+
+  handleChangeName = (e) => {
+    const { value } = e.target;
+    this.setState({ name: value });
   };
 
   render() {
-    console.log(this.state.idValue);
+    const { id, pw, name, passwordcheck } = this.state;
+    console.log(id, pw, name, passwordcheck);
+
     return (
       <div className="page">
         <section className="container">
@@ -31,12 +82,11 @@ class Signup extends Component {
               <div className="email">이메일</div>
               <input
                 className="emailInput"
-                type="text"
+                type="email"
+                name="email"
                 placeholder="이메일"
-                onChange={(e) => this.handleChangeEmail(e)}
+                onChange={this.handleChangeEmail}
               />
-              <span className="at">@</span>
-              <Select />
             </section>
             <section className="pwAddress">
               <div className="pw">비밀번호</div>
@@ -45,7 +95,7 @@ class Signup extends Component {
                 className="pwInput"
                 type="password"
                 placeholder="비밀번호"
-                onChange={this.handleInputChange}
+                onChange={this.handleChangePw}
               />
             </section>
             <section className="rePwAddress">
@@ -54,7 +104,7 @@ class Signup extends Component {
                 className="rePwInput"
                 type="password"
                 placeholder="비밀번호 확인"
-                onChange={this.handleInputChange}
+                onChange={this.handleChangePwCheck}
               />
             </section>
             <section className="nickname">
@@ -68,7 +118,7 @@ class Signup extends Component {
                 type="text"
                 placeholder="별명 (2~15자)"
                 value={this.state.nickname}
-                onChange={this.handleNickname}
+                onChange={this.handleChangeName}
               />
             </section>
             <section className="agree">
@@ -105,7 +155,11 @@ class Signup extends Component {
                 </div>
               </div>
             </section>
-            <button className="signupButton" onKeyPress={this.checkValidation}>
+            <button
+              className="signupButton"
+              // onKeyPress={this.checkValidation}
+              onClick={(e) => this.handleClick(e)}
+            >
               회원가입 완료
             </button>
             <div className="alreadySignup">
@@ -118,5 +172,4 @@ class Signup extends Component {
     );
   }
 }
-
 export default Signup;
