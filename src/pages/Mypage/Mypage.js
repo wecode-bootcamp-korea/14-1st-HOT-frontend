d0c328338ab311892a41d818b939873efed6dae7
공주민 <ley67890@naver.com>
2020-11-23 10:46:06 +0900

    ADD : Mypage 추가

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
+import "./Mypage.scss";
 
 class Mypage extends Component {
   render() {
     return (
       <section className="Mypage">
-        <div className="userProfile">
+        <div className="bigContainer">
           <div className="userProfileContainer">
-            <img src="./images/user.png" alt="user" />
-            <div className="userInfo">
-              <div>H.O.T</div>
-              <div className="following">
-              <span>팔로워 0</span>
-              <span>팔로잉 0</span>
+            <div className="userProfile">
+              <div>
+                <img className="userImg" src="./images/user.png" alt="user" />
               </div>
-              <button
+              <div className="userInfo">
+                <div className="username">wecode H.O.T</div>
+                <div className="following">
+                  <span className="follower">팔로워 492</span>
+                  <span className="line">|</span>
+                  <span className="following">팔로잉 285</span>
+                  <br></br>
+                </div>
+                <button className="button">설정</button>
+              </div>
+            </div>
+            <div className="userLink">
+              <div className="scrap">
+                <img
+              </div>
             </div>
           </div>
         </div>
