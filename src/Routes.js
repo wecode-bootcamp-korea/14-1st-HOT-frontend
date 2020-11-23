7cc8ea2d71e242dc429cab3cc83c6167042cb929
yoonl <kyouk12a@gmail.com>
2020-11-23 11:19:45 +0900

    Fixed: conflict 해결

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Community from "./pages/Community/Community";
-import FeedList from "./pages/Community/FeedList/FeedList";
-
-import Category from "./pages/Category/Category";
 
 class Routes extends React.Component {
   render() {
     return (
-      <>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={FeedList} />
-
-            <Route exact path="/" component={Category} />
-          </Switch>
-        </Router>
-      </>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import './styles/common.scss';
-import './styles/reset.scss';
-import './config';
-import './pages/Category/Category.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import "./styles/common.scss";
+import "./styles/reset.scss";
+import "./config";
+import "./pages/Category/Category.scss";
 
 ReactDOM.render(<Routes />, document.getElementById("thou"));
