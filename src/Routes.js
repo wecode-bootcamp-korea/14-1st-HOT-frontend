fa5549573de0554e6b862f57c45a7ef5cec705df
공주민 <ley67890@naver.com>
2020-11-25 08:50:16 +0900

    Modify : Signup cscc 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Community from "./pages/Community/Community";
 import Signup from "./pages/Signup/Signup";
 
 class Routes extends React.Component {
@@ -8,7 +7,6 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Community} />
           <Route exact path="/signup" component={Signup} />
         </Switch>
       </Router>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Signup.scss";
+import { Link } from "react-router-dom";
 import Simplesignup from "./Simplesignup.js";
+import Agree from "./Agree";
 
 const API = "http://10.58.3.106:8000/user/register";
 
@@ -22,7 +24,7 @@ class Signup extends Component {
   handleClick = (e) => {
     const { id, pw, name } = this.state;
 
-    fetch("http://10.58.3.106:8000/user/register", {
+    fetch(API, {
       method: "POST",
       body: JSON.stringify({
         email: id,
@@ -33,12 +35,12 @@ class Signup extends Component {
       .then((res) => res.json())
       .then((result) => console.log(result));
 
-    // if (this.state.data.message === "success") {
-    //   alert("회원가입이 완료되었습니다.");
-    //   this.props.history.push("/");
-    // } else {
-    //   alert(this.state.data.message);
-    // }
+    if (this.state.data.message === "success") {
+      alert("회원가입이 완료되었습니다.");
+      this.props.history.push("/");
+    } else {
+      alert(this.state.data.message);
+    }
   };
 
   handleChangeEmail = (e) => {
@@ -66,8 +68,8 @@ class Signup extends Component {
     console.log(id, pw, name, passwordcheck);
 
     return (
-      <div className="page">
-        <section className="container">
+      <div className="Signup">
+        <section className="userContainer">
           <div className="logo">
             <img className="thou" src="./images/logo.png" alt="logo" />
             <img className="text" src="./images/logo_text.png" alt="text" />
@@ -117,40 +119,7 @@ class Signup extends Component {
                 onChange={this.handleChangeName}
               />
             </section>
-            <section className="agree">
-              <div className="contentAgree">약관 동의</div>
-              <div className="agreeContainer">
-                <div className="userSignup">
-                  <input className="checkBox" type="checkbox" />
-                  <span className="allAgree">전체동의</span>
-                </div>
-                <div className="fourteen">
-                  <input className="checkBox" type="checkbox" />
-                  <span className="addcontent">만 14세 이상입니다.</span>
-                  <span className="essential">(필수)</span>
-                </div>
-                <div className="useAgree">
-                  <input type="checkbox" />
-                  <span className="checkBox" type="checkbox" />
-                  <span className="undercontent">이용약관</span>
-                  <span className="essential">(필수)</span>
-                </div>
-                <div className="private">
-                  <input className="boxbox" type="checkbox" />
-
-                  <span className="undercontent">개인정보처리방침</span>
-                  <span className="essential">(필수)</span>
-                </div>
-                <div className="event">
-                  <input type="checkbox" />
-                  <span className="checkBox" type="checkbox" />
-                  <span className="addcontent">
-                    이벤트, 프로모션 알림 메일 및 SMS 수신
-                  </span>
-                  <span className="choice">(선택)</span>
-                </div>
-              </div>
-            </section>
+            <Agree />
             <button
               className="signupButton"
               // onKeyPress={this.checkValidation}
@@ -160,7 +129,9 @@ class Signup extends Component {
             </button>
             <div className="alreadySignup">
               <span className="haveId">이미 아이디가 있으신가요?</span>
-              <span className="reLogin">로그인</span>
+              <Link className="reLogin" to="/login">
+                로그인
+              </Link>
             </div>
           </section>
         </section>
