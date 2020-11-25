3e78ed033f5d74b30005dacffe87702757570cff
Xednicoder <kaaiinn4@gmail.com>
2020-11-26 02:33:13 +0900

    Modify : review

--- src/pages/Category/ListOther/ListOther.js
+++ src/pages/Category/ListOther/ListOther.js
@@ -4,12 +4,11 @@ import './ListOther.scss';
 class ListOther extends Component {
   render() {
     const { other } = this.props;
-    console.log(this.props.other);
     return (
       <>
         <div className='otherListBox'>
           {other.map((otherElement, otherIndex) => (
-            <div className='' key={otherIndex}>
+            <div className='' key={otherIndex} onClick={otherElement}>
               <div className='otherList'>
                 <span>{otherElement.menu_name}</span>
               </div>
