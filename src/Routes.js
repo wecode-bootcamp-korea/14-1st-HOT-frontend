d952f84b2c75a36c1ae8c21ba71f575c7bb78126
Xednicoder <kaaiinn4@gmail.com>
2020-11-26 07:22:00 +0900

    Fixed : Server error

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Category from './pages/Category/Category';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
 // import ProductList from './pages/Category/ProductList/ProductList';
 // import Signup from './pages/Signup/Signup';
 // import Login from './pages/Login/Login';
@@ -11,7 +12,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={Category} />
+          <Route exact path='/categories' component={Category} />
+          <Route exact path='/categories/:id' component={ProductDetail} />
           {/* <Route exact path='/signup' component={Signup} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/productlist' component={ProductList} />
