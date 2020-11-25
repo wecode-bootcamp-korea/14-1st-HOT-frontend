ab459556d6a7a6e30d60da10e414fdcd3798242d
공주민 <ley67890@naver.com>
2020-11-25 09:53:38 +0900

    Modify : 리뷰 반영

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavigationBar from "./component/NavigationBar/NavigationBar";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/nav" component={NavigationBar} />
+          <Route exact path="/nav" component="" />
         </Switch>
       </Router>
     );
