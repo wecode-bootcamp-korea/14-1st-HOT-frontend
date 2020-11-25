f4cd93074f3220c871bd0c3a4923f23247e03f26
공주민 <ley67890@naver.com>
2020-11-25 09:51:37 +0900

    Modify : 리뷰 반영

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component="" />
         </Switch>
       </Router>
     );
