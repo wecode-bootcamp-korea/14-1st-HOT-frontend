63f739d1183bd6d28036eec7dbdb3e7b05f55d06
공주민 <ley67890@naver.com>
2020-11-26 15:24:06 +0900

    Modify : Routes 수정

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/nav" component="{NavigationBar}" />
+          <Route exact path="/" component="" />
         </Switch>
       </Router>
     );
