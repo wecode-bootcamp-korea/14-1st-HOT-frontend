fa5471b6151b21f4d5295bb53149c7796599f289
공주민 <ley67890@naver.com>
2021-01-19 18:01:21 +0900

    [modify] mypage API 수정

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './Mypage.scss';
-import MypageFeed from './MypageFeed';
-import Userlink from './Userlink';
-// import { API } from '../../../config';
+import React, { Component } from "react";
+import "./Mypage.scss";
+import MypageFeed from "./MypageFeed";
+import Userlink from "./Userlink";
+import { MYPAGE } from "../../config";
 
 class Mypage extends Component {
   constructor(props) {
@@ -15,15 +15,11 @@ class Mypage extends Component {
   }
 
   componentDidMount() {
-    fetch('http://13.125.3.178:8000/user/mypage', {
-      headers: {
-        Authorization: localStorage.getItem('token'),
-      },
-    })
+    fetch(MYPAGE)
       .then((response) => response.json())
       .then((res) => {
         this.setState({ result: res.context }, () =>
-          console.log('re', this.state.result)
+          console.log("re", this.state.result)
         );
         this.setState({
           myPageImage: this.state.result.bookmark_products,
@@ -36,37 +32,37 @@ class Mypage extends Component {
     const { myPageImage, bookmark_count } = this.state;
 
     return (
-      <section className='Mypage'>
-        <div className='bigContainer'>
-          <div className='profileContainer'>
-            <div className='userProfileContainer'>
-              <div className='share'>
-                <img src='./images/share.png' alt='share' />
+      <section className="Mypage">
+        <div className="bigContainer">
+          <div className="profileContainer">
+            <div className="userProfileContainer">
+              <div className="share">
+                <img src="./images/share.png" alt="share" />
               </div>
-              <div className='userProfile'>
-                <div className='userimage'>
-                  <img src='./images/wecode.jpeg' alt='user' />
+              <div className="userProfile">
+                <div className="userimage">
+                  <img src="./images/wecode.jpeg" alt="user" />
                 </div>
-                <div className='userInfo'>
-                  <div className='username'>wecode</div>
-                  <div className='following'>
-                    <span className='follower'>팔로워</span>
-                    <span className='followerNumber'>492</span>
-                    <span className='line'>|</span>
-                    <span className='following'>팔로잉</span>
-                    <span className='followingNumber'>285</span>
+                <div className="userInfo">
+                  <div className="username">wecode</div>
+                  <div className="following">
+                    <span className="follower">팔로워</span>
+                    <span className="followerNumber">492</span>
+                    <span className="line">|</span>
+                    <span className="following">팔로잉</span>
+                    <span className="followingNumber">285</span>
                     <br></br>
                   </div>
-                  <button className='button'>설정</button>
+                  <button className="button">설정</button>
                 </div>
               </div>
               <Userlink bookmark_count={bookmark_count} />
             </div>
-            <div className='getCoupon'>
-              <span className='add'>친구 초대하고</span>
-              <span className='point'>5,000P</span>
-              <span className='get'>받기</span>
-              <img className='arrow' src='./images/right.png' alt='arrow' />
+            <div className="getCoupon">
+              <span className="add">친구 초대하고</span>
+              <span className="point">5,000P</span>
+              <span className="get">받기</span>
+              <img className="arrow" src="./images/right.png" alt="arrow" />
             </div>
           </div>
           <MypageFeed myPageImage={myPageImage} />
